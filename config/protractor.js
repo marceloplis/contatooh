d7dd9f8a4c1d7bec87fd8a64a3d33f7be3eca4cb
Marcelo <marceloplis@yahoo.com.br>
2017-02-07 11:03:07 -0200

    primeiro commit

---
+++ config/protractor.js
@@ -0,0 +1,13 @@
+/**
+ * http://usejsdoc.org/
+ */
+exports.config = {
+		specs: ['../test/e2e/**/*Spec.js'],
+		onPrepare: function() {
+				browser.driver.get('http://localhost:3000');
+				browser.driver.findElement(by.id('entrar')).click();
+				browser.driver.findElement(by.id('login_field')).sendKeys('email-de-teste');
+				browser.driver.findElement(by.id('password')).sendKeys('senha-do-email-de-teste');
+				browser.driver.findElement(by.name('commit')).click();
+		}
+};
