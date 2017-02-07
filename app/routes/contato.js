dfc8cddb193037a1c96d8d52fb325a05ea363aba
marceloplis <marceloplis@yahoo.com.br>
2017-02-07 15:28:49 -0200

    Update contato.js

--- app/routes/contato.js
+++ app/routes/contato.js
@@ -11,11 +11,11 @@ module.exports = function(app) {
 	var controller = app.controllers.contato;
 
 	app.route('/contatos')
-		.get(/*verificaAutenticacao,*/controller.listaContatos)
-		.post(/*verificaAutenticacao,*/controller.salvaContato);
+		.get(verificaAutenticacao,controller.listaContatos)
+		.post(verificaAutenticacao,controller.salvaContato);
 
 	app.route('/contatos/:id')
-		.get(/*verificaAutenticacao,*/controller.obtemContato)
-		.delete(/*verificaAutenticacao,*/controller.removeContato);
+		.get(verificaAutenticacao,controller.obtemContato)
+		.delete(verificaAutenticacao,controller.removeContato);
 
 };
